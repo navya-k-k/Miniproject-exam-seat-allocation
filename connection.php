29e692cb5c80654258e2a7f6831e0a04f151a8cf
navya-k-k <75126858+navya-k-k@users.noreply.github.com>
2022-02-20 17:44:53 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$dbcon=mysqli_connect("localhost","root","","notice-board");
+//mysql_select_db("student_placement");
+$title="COLLEGE OF ENGINEERING TRIVANDRUM";
+$hd="COLLEGE OF ENGINEERING TRIVANDRUM";
+$footr="<span class='blue bolder'>Project</span> &copy;";
+?>
